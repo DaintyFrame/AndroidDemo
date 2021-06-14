bcbaf8bbe0443fc71774e02c40956d0ee1e71f8e
DaintyFrame <Josephin.Esc@mail.com>
2021-06-14 12:26:07 +0800

    test

--- app/src/main/java/example/androiddemo/ui/MainActivity.java
+++ app/src/main/java/example/androiddemo/ui/MainActivity.java
@@ -5,11 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 
 import example.androiddemo.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+
+
+
 }
